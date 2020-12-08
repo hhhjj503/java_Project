696a5358ce96775811846a9a6b74205826ab8495
nav <hhj@naver.com>
2020-12-08 23:35:03 +0900

    20201208

---
+++ JavaProject/src/ex1/io/Program.java
@@ -0,0 +1,18 @@
+package ex1.io;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.write('a' +0);
+		System.out.write('a' +1);
+		System.out.write('a' +2);
+		System.out.write('a' +3);
+		System.out.flush(); 
+		
+		System.out.println(); //write + flush
+	}
+
+}
